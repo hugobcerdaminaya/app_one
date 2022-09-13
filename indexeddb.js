11d2f49abcb931e4d4987d2a9a8db159d761a979
Hugo B. Cerda Minaya <hcerda@strappcorp.com>
2022-09-13 15:33:42 -0500

    add dixie.js test

--- indexeddb.js
+++ indexeddb.js
@@ -2,6 +2,55 @@ const indexedDB = window.indexedDB
 const form = document.getElementById("form")
 const esquemas = document.getElementById("esquemas")
 
+//Example use dexie.js  
+const db = new Dexie("FriendDatabase");
+db.version(1).stores({
+  friends: `
+    id,
+    name,
+    age`,
+});
+
+// Now add some values.
+db.friends.bulkPut([
+    { id: 1, name: "Josephine", age: 21 },
+    { id: 2, name: "Per", age: 75 },
+    { id: 3, name: "Simon", age: 5 },
+    { id: 4, name: "Sara", age: 50, notIndexedProperty: 'foo' }
+  ]).then(() => {
+
+    return db.friends.where("age").between(0, 25).toArray();
+
+  }).then(friends => {
+
+    console.log("Found young friends: " +
+      friends.map(friend => friend.name));
+
+    return db.friends
+      .orderBy("age")
+      .reverse()
+      .toArray();
+
+  }).then(friends => {
+
+    console.log("Friends in reverse age order: " +
+      friends.map(friend => `${friend.name} ${friend.age}`));
+
+    return db.friends.where('name').startsWith("S").keys();
+
+  }).then(friendNames => {
+
+    console.log("Friends on 'S': " + friendNames);
+
+  }).catch(err => {
+
+    console.log("Ouch... " + err);
+
+  });
+
+//Example use dexie.js  
+
+
 if(indexedDB && form){
     let db
     const request = indexedDB.open('ListasEsquemas', 1)
